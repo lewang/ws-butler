00e9ca4aceb922329172d7af9420e593cf522415
Le Wang <le.wang@agworld.com.au>
2013-01-05 21:50:58 +0800

    commentary update

--- ws-butler.el
+++ ws-butler.el
@@ -1,3 +1,58 @@
+;;; ws-butler.el --- unobstrusively remove trailing whitespace
+
+;; this file is not part of Emacs
+
+;; Copyright (C) 2013 Le Wang
+;; Author: Le Wang
+;; Maintainer: Le Wang
+;; Description: unobstrusively remove trailing whitespace
+;; Author: Le Wang
+;; Maintainer: Le Wang
+
+;; Created: Sat Jan  5 16:49:23 2013 (+0800)
+;; Version: 0.1
+;; Last-Updated:
+;;           By:
+;;     Update #: 2
+;; URL: https://github.com/lewang/ws-butler
+;; Keywords:
+;; Compatibility: Emacs 24
+
+;;; Installation:
+
+;;
+;;
+;;
+
+;;; Commentary:
+
+;;
+;;
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+
 (defun ws-butler-trim-region (beg end)
   "Delete trailing blanks in region BEG END."
   (interactive "*r")
@@ -94,3 +149,6 @@ save."
     (remove-hook 'after-revert-hook 'ws-butler-after-save t)
     (remove-hook 'edit-server-done-hook 'ws-butler-before-save t)))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ws-butler.el ends here
